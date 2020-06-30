0ce4f8f1323366292c09b5bd9b0d1b852b44de21
UnityDo <63395346+UnityDo@users.noreply.github.com>
2020-06-30 18:23:14 +0200

    Paquete

---
+++ com.jorgevallejo.utilidad/Runtime/Utilidad.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace JorgeVallejo.Core
+{
+    public class Utilidad : MonoBehaviour
+    {
+        public const int color = 123;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
